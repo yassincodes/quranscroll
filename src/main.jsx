52f722dbd32af25650174af0e4be75fde2744c24
mohmmdyassin <mohammedyassinkhalifi@gmail.com>
2026-01-03 10:31:23 -0800

    .

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App.jsx';
+
+// Import Analytics
+import { Analytics } from '@vercel/analytics/react';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
+    <Analytics /> {/* Add this here */}
   </StrictMode>,
-)
+);
